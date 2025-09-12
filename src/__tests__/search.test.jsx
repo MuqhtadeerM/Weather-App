0d797159f0bd7270d72619614cec20247620f888
BeingmuqhtadeerM <muhammedmuzawar9@gmail.com>
2025-09-13 01:21:06 +0530

    Testing Search

--- src/__tests__/search.test.jsx
+++ src/__tests__/search.test.jsx
@@ -1,10 +1,16 @@
 import Search from "../Components/search";
 
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test("testing search", () => {
-  render(<Search />);
+test("testing search and user input data", async () => {
+  //creating mock function
+  const mockSearch = vi.fn();
+  const mockHandleSearch = vi.fn();
+  render(
+    <Search search="" setSearch={mockSearch} handleSearch={mockHandleSearch} />
+  );
 
   // Role Text Box
   const input = screen.getByRole("textbox");
@@ -14,6 +20,13 @@ test("testing search", () => {
   expect(input).toHaveAttribute("placeholder", "Enter City Name");
 
   //check name att
-
   expect(input).toHaveAttribute("name", "search");
+
+  //   initial value should be
+  expect(input).toHaveValue("");
+
+  // simulate usertyping
+
+  await userEvent.type(input, "Bengaluru");
+  expect(mockSearch).toHaveBeenCalled("Bengaluru".length);
 });
